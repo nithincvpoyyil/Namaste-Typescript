6468ac6dcbd08aa2d01c810a4127dd537b369fe3
nithincvpoyyil <nithincv1@gmail.com>
2016-02-14 23:02:21 +0530

    added proper comments

--- app-src/baseView.ts
+++ app-src/baseView.ts
@@ -6,24 +6,26 @@
 import {CrGenericEventHandler} from './genericEventHandler';
 
 export class BaseView implements CrGenericEventHandler {
-
+  // container class
   private container: any;
+  //event collection of binded events
   eventCollection: { event: string; handler: Function; data: any }[];
+
   constructor(container) {
     this.container = container;
     this.eventCollection = [];
   }
-
+  // 'on' method implementation of Interface
   on(type: string, handler: Function, data?: any): void {
     this.eventCollection.push({ "event": type, "handler": handler, "data": data });
   }
-
+  // 'off' method implementation of Interface
   off(type: string): void {
     this.eventCollection = this.eventCollection.filter(function(eventManageItem) {
       return (eventManageItem["event"] !== type)
     });
   }
-
+  //'trigger' method implementation of Interface
   trigger(type: string, data?: any): void {
     var self = this;
     self.eventCollection.forEach(function(eventManageItem) {
@@ -32,13 +34,13 @@ export class BaseView implements CrGenericEventHandler {
       }
     });
   }
-
+// Getter for container node
   getContainer(): any {
     return this.container;
   }
-
+  //logger method for debugging
   logger(msg: any): void {
     console.info(new Date().getTime() + " >>" + msg)
   }
-  
+
 }
